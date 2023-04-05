b834798f2536544bc865d43918a84561feb60b27
azay10 <117178019+azay10@users.noreply.github.com>
2023-04-05 08:48:34 +0545

    Add files via upload

---
+++ 11/polymorphism.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public abstract class Animal
+    {
+        public abstract void WhatIeat();
+        public abstract void WhatIbreathe();
+        //{
+        //    Console.WriteLine("I breathe oxygen");
+        //}
+
+    }
+    public partial class Tiger : Animal
+    {
+        public override void WhatIeat()
+        {
+            Console.WriteLine("I eat flesh");
+        }
+    }
+    public partial class  Tiger : Animal
+    {
+        public override void WhatIbreathe()
+        {
+            Console.WriteLine("I eat flesh");
+        }
+    }
+    //public interface Animals
+    //{
+    //    void WhatIeat();
+    //    void WhatIbreathe();
+    //}
+    //public class Lion : Animal
+    //{
+    //    public override void WhatIeat()
+    //    {
+    //        Console.WriteLine("I eat flesh");
+    //    }
+    //}
+    internal class polymorphism
+    {
+        public void WhoAmI()
+        {
+            Console.WriteLine("I am empty function");
+
+        }
+        public void WhoAmI(string name)
+        {
+            Console.WriteLine("I am" + name);
+        }
+        public void WhoAmI(int myRollNumber)
+        {
+            Console.WriteLine("my roll number is" + myRollNumber);
+        }
+
+    }
+}
